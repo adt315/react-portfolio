53d39f05810aad566e9af5072e19941e0f147e01
adt315 <angela_dyan@hotmail.com>
2021-01-20 07:55:12 -0500

    add contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,13 @@ function Contact() {
     return (
       <div>
           <h1 className="text-center">Contact Me</h1>
-          {/* <ContactButton /> */}
+          <div className="contact-card text-center">
+            <h5>GitHub: <a href="https://github.com/adt315">github.com/adt315</a></h5>
+            <h5>LinkedIn: <a href="https://www.linkedin.com/in/angela-tooley-7107468/">linkedin.com/in/angela-tooley-7107468</a></h5>
+            <h5>Email:  angela_dyan@hotmail.com</h5>
+            <h5>Phone:  (817)944-9024</h5>
+          </div>
+          
       </div>
     );
   }
